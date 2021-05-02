b70357c5ab0e5b4f4a36561e3007ffe74b30d74c
xxbijanxx <bijanxx@gmail.com>
2021-05-02 11:57:38 -0700

    Updated Main File

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import random
 from word_bank import Word_Bank
 from tkinter import *
 
+wb = Word_Bank()
+wb.addWord("Hola", "Hello")
+wb.addWord("Como", "How")
+wb.addWord("Adios", "Bye")
+wb.addWord("Amigo", "Friend")
+wb.addWord("Comida", "Food")
+wb.addWord("La Playa", "Beach")
+wb.addWord("El Bosque LLuvioso", "The Rainforest")
+
+
 def deal_card(wordbank):
     word = random.choice(wordbank)
-    shown_words.append(word)
+
+
 
 
 def next_card():
-    print("Next Card Coming Up!")
+    current_card = random.choice(wb.words)
+    key_list = list(wb.words.keys())
+    # word_key = random.choice(wb.words)
+    print(key_list)
+
+    canvas.itemconfig(card_english, text="English Test")
+    canvas.itemconfig(card_spanish, text="Spanish Test")
 
 
 
@@ -30,8 +47,8 @@ canvas = Canvas(width=MAX_WIDTH, height=MAX_HEIGHT)
 
 card_front_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(MIDDLE_X_CANVAS, MIDDLE_Y_CANVAS, image=card_front_img)
-canvas.create_text(MIDDLE_X_CANVAS, MIDDLE_Y_CANVAS - 113, text="Title", font=("Arial", 40, "italic"))
-canvas.create_text(MIDDLE_X_CANVAS, MIDDLE_Y_CANVAS, text="Word", font=("Arial", 40, "italic"))
+card_english = canvas.create_text(MIDDLE_X_CANVAS, MIDDLE_Y_CANVAS - 113, text="English", font=("Arial", 40, "italic"))
+card_spanish = canvas.create_text(MIDDLE_X_CANVAS, MIDDLE_Y_CANVAS, text="Spanish", font=("Arial", 60, "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 wrong_img = PhotoImage(file="./images/wrong.png")
